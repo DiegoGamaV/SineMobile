43b69e0017c88ffdeb723ec5c3246db4e8208922
RaylaMedeiros <Rayla.Medeiiros@gmail.com>
2016-10-30 21:25:41 -0300

    - Splash Screen.

---
+++ SineMobile/app/src/main/java/br/edu/ifpb/cg/info/sinemobile/Activities/SplashScreen.java
@@ -0,0 +1,32 @@
+package br.edu.ifpb.cg.info.sinemobile.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import br.edu.ifpb.cg.info.sinemobile.R;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
